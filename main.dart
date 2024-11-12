da2996d6b865f9b9581acafd9fc110406c7c585f
yogasetyawan <astdnn48@gmail.com>
2024-11-12 12:07:00 +0700

    coding v2

--- main.dart
+++ main.dart
@@ -8,123 +8,61 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Login App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: LoginPage(),
-    );
-  }
-}
-
-class LoginPage extends StatelessWidget {
-  final TextEditingController emailController = TextEditingController();
-  final TextEditingController passwordController = TextEditingController();
-  final TextEditingController ForgotController = TextEditingController();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Login Page'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset('Daco_1834544.png',
-            width: 200,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Container Text Example'),
+        ),
+        body: Center(
+          child: Container(
+            width: double.infinity,
+            margin: const EdgeInsets.all(12),
             height: 200,
-            ),
-            TextField(
-              controller: emailController,
-              decoration: InputDecoration(
-                labelText: 'Email',
-                border: OutlineInputBorder(),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(12),
+              gradient: const LinearGradient(
+                colors: [
+                  Color.fromARGB(255, 0, 0, 0),
+                  Color.fromARGB(255, 60, 68, 158),
+                  Color.fromARGB(255, 60, 68, 158),
+                  Color.fromARGB(255, 0, 0, 0),
+                ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
               ),
             ),
-            SizedBox(height: 16.0),
-            TextField(
-              controller: passwordController,
-              decoration: InputDecoration(
-                labelText: 'Password',
-                border: OutlineInputBorder(),
+            child: Padding(
+              padding: const EdgeInsets.all(20), // Adjust padding as needed
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start, // Left-align text
+                children: [
+                  const Text(
+                    'membuat container widget',
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  const Divider(color: Colors.black),
+                  const Text(
+                    'halo rowrrrrrr bisa juga akhirnya buat container',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  const SizedBox(height: 20),
+                  SizedBox(
+                    height: 40,
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: const Text('jangan di klik'),
+                    ),
+                  ),
+                ],
               ),
-              obscureText: true,
             ),
-            SizedBox(height: 16.0),
-            
-            Container(
-  width: 100,
-  child: Text(overflow: TextOverflow.ellipsis, 'buat akun?')),
-            ElevatedButton(
-              onPressed: () {
-                
-                String email = emailController.text;
-                String password = passwordController.text;
-                print('Email: $email, Password: $password');
-
-                if (email.isEmpty || password.isEmpty) {
-                 ScaffoldMessenger.of(context).showSnackBar(
-                 SnackBar(content: Text('Email dan Password tidak boleh kosong!')),
-                );
-                return;
-                 }
-
-              },
-              child: Text('Login'),
-            ),
-          ],
+          ),
         ),
-      ),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
